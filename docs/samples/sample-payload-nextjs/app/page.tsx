95c230ef7c84e67cb8b18dc2b4078ab844013343
kgremban <kgremban@microsoft.com>
2024-01-17 15:46:16 -0800

    Rename and reorganize docs folder structure

--- docs/samples/sample-payload-nextjs/app/page.tsx
+++ docs/samples/sample-payload-nextjs/app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
           </CardBody>
           <Divider/>
           <CardFooter>
-            <Link isExternal showAnchorIcon href="https://github.com/eclipse-symphony">
+            <Link isExternal showAnchorIcon href="https://github.com/azure/symphony">
               Visit Symphony source code on GitHub.
             </Link>
           </CardFooter>
